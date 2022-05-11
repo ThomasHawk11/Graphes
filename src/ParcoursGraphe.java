83a0161a74590d859b61d2c611f81649e990a657
ThomasHawk <94489158+ThomasHawk11@users.noreply.github.com>
2022-05-11 17:11:49 +0200

    Update ParcoursGraphe.java

--- src/ParcoursGraphe.java
+++ src/ParcoursGraphe.java
@@ -1,14 +1,21 @@
+package sae;
+
 import sae2_2.Graphe;
 
 public abstract class ParcoursGraphe {
-	
-    public ParcoursGraphe(Graphe g) {}
-    
-    public abstract boolean existeChemin(String a, String b);
-    
-    public abstract String plusCourtChemin(String a, String b);
-    
-    public abstract Graphe getGraphe();
+    private Graphe graphe;
 
-	
+    public ParcoursGraphe(Graphe g) {
+        this.graphe = g;
+    }
+
+    public static boolean existeChemin(String a, String b) {
+        return false;
+    }
+
+    public abstract String plusCourtChemin(String a, String b);
+
+    public abstract Graphe getGraphe();
 }
+
+
