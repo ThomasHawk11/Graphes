b04719a9962156f843a36dca390e9762e927589c
ThomasHawk <94489158+ThomasHawk11@users.noreply.github.com>
2022-05-11 17:12:51 +0200

    Update ParcoursGrapheLargeur.java

--- src/ParcoursGrapheLargeur.java
+++ src/ParcoursGrapheLargeur.java
@@ -1,27 +1,54 @@
+package sae;
+
 import sae2_2.Graphe;
+import sae2_2.Voisin;
+import sae2_2.Arete;
+import sae.GrapheMatrice;
+import sae.ParcoursGraphe;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 
 public class ParcoursGrapheLargeur extends ParcoursGraphe {
+    private Graphe graphe;
 
-	public ParcoursGrapheLargeur(Graphe g) {
-		super(g);
-	}
+    public ParcoursGrapheLargeur(Graphe g) {
+        this.graphe=g;
+    }
 
-	@Override
-	public boolean existeChemin(String a, String b) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public String plusCourtChemin(String a, String b) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Graphe getGraphe() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+    public static boolean existeChemin(String a, String b) {
+        if(a==b) return true;
+        Queue<String> queue = new LinkedList<String>();
+        queue.add(a);
+        ArrayList<String> visite = new ArrayList<String>();
+        String temp;
+        while(!queue.isEmpty()){
+            temp = queue.remove();
+            visite.add(temp);
+            for(Voisin v : _______.getVoisins(temp)){
+                if(!visite.contains(v.getEtiquette())){
+                    if(v.getEtiquette().equals(b))return true;
+                    else{
+                        visite.add(v.getEtiquette());
+                        queue.add(v.getEtiquette());
+                    }
+                }
+            }
+        }
+ return false;
 }
+
+    @Override
+    public String plusCourtChemin(String a, String b) {
+        return null;
+    }
+
+    @Override
+    public Graphe getGraphe() {
+        return ;
+    }
+}
+
